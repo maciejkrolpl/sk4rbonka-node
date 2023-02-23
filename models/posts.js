189844450ab67be2007e2fe911b63c988cdf57dd
Maciej Król <maciej.krol@awesomebytes.pl>
2023-02-23 15:47:27 +0100

    remove mongoose leftovers

--- models/posts.js
+++ models/posts.js
@@ -1,9 +1,1 @@
-import mongoose from 'mongoose';
-const postSchema = new mongoose.Schema({
-  'title': String,
-  'content': String,
-  'author': String,
-  'tags': [String],
-}, { timestamps: true });
-const Post = mongoose.model('post', postSchema);
-export default Post;
+export default {};
