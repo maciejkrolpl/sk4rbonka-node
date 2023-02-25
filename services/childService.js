a5f8418bed25013070ef69014ade159aea8e7174
Maciej Król <maciej.krol@awesomebytes.pl>
2023-02-25 10:27:06 +0100

    typo

--- services/childService.js
+++ services/childService.js
@@ -2,7 +2,7 @@ import client from "../db/db.js";
 import createNanoID from "../utils/nanoId.js";
 
 export const queryAllChildren = async () => {
-  const query = 'SELECT child_id, name, balance FROM childrenx';
+  const query = 'SELECT child_id, name, balance FROM children';
   const result = await client.query(query);
   const { rows } = result;
   return rows;
